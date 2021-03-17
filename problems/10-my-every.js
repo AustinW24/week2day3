8f78f700b67522422a15b454e417e8b36bf5469b
Austin Williams <austinwill264@gmail.com>
2021-03-17 15:20:17 -0700

    completed 10

--- problems/10-my-every.js
+++ problems/10-my-every.js
@@ -19,14 +19,42 @@ console.log(myEvery([4, 8, 6, 10], isEven));            // true
 console.log(myEvery([4, 7, 6, 10], isEven));            // false
 console.log(myEvery(['book', 'door'], hasO));           // true
 console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
+
+Write a function `myEvery` that accepts an array and a callback as arguments.
+The function should return a boolean indicating whether or not all elements of
+the array return true when passed into the callback.
 *******************************************************************************/
 
-let myEvery = function() {
+//do
+//array and cb params
+//set filter function to a variable
+//use filter function on array
+//if callback equates to true
+//return true
+//if filter variable === array
+//return true
 
+let myEvery = function(array, cb) {
+for(let i = 0; i < array.length; i++) {
+    if(cb(array[i]) === false) {
+        return false;
+    };
+}
+return true
 };
 
+let isEven = function (num) {
+    return num % 2 === 0;
+};
 
+let hasO = function(string) {
+    return string.includes('o');
+};
 
+console.log(myEvery([4, 8, 6, 10], isEven));            // true
+console.log(myEvery([4, 7, 6, 10], isEven));            // false
+console.log(myEvery(['book', 'door'], hasO));           // true
+console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 
 
 
