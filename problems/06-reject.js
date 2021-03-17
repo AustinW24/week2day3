ba0ca82a19fec02b38cfe27b373ce6cf1e318644
Austin Williams <austinwill264@gmail.com>
2021-03-17 13:53:21 -0700

    complete 05, 06

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -19,12 +19,40 @@ let hasA = function(s) {
     return s.toLowerCase().includes('a');
 };
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
+
+Write a function `reject` that accepts an array and callback as arguments. The
+function should call the callback for each element of the array, passing in the
+element. The function should return a new array
+containing elements of the original array that result in false when given to the
+callback.
+
 *******************************************************************************/
-
-let reject = function() {
-
+//do
+//create an array and callback parm
+//init empty variable array
+//loop through array
+//if cb returns false, push current element into empty array
+//call each element with callback
+//return array
+let reject = function(array, cb1) {
+var result = [];
+for(var i = 0; i < array.length; i++) {
+    let el = array[i];
+    if(!cb1(el)) {
+        result.push(el)
+    }
+}
+return result;
 };
+let isEven = function(n) {
+    return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
+let hasA = function(s) {
+    return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 
 
 
--- problems/07-my-some.js
+++ problems/07-my-some.js
@@ -21,14 +21,37 @@ let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function(ele) {
     return ele.length === 4;
 });
 console.log(result3);   // true
-*******************************************************************************/
 
+Write a function `mySome` that accepts an array and a callback as an argument.
+The function should call the callback for each element of the array, passing in
+the element and its index. The function should return a boolean
+indicating whether or not at least one element of the array returns true when passed
+into the callback.
+*******************************************************************************/
+//do
+//create array and cb params
+////loop through array
+//create alias for el
+//if el and its index return true
 let mySome = function() {
 
 };
 
 
+let result1 = mySome([5, 1, 7, 9], function(ele, i) {
+    return ele === i;
+});
+console.log(result1);   // true
 
+let result2 = mySome([5, 3, 7, 9], function(ele, i) {
+    return ele === i;
+});
+console.log(result2);   // false
+
+let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function(ele) {
+    return ele.length === 4;
+});
+console.log(result3);   // true
 
 
 
