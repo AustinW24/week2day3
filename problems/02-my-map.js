8992e5c2cf8621bc9903cce5abb5b66ca49bff2d
Austin Williams <austinwill264@gmail.com>
2021-03-17 13:05:58 -0700

    complete 01, 02

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -14,13 +14,33 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
     return el.toUpperCase() + '!';
 });
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
+
+Write a function `myMap` that accepts an array and a callback as arguments.
+The function return an array of new elements obtained by calling the callback on
+each element of the array, passing in the element.
+
+Do not use the built in Array#map
 *******************************************************************************/
 
-let myMap = function() {
+let myMap = function(array, cb) {
+var myArr = [];
+for(var i = 0; i < array.length; i++) {
+    var el = array[i];
 
+    let result = cb(el, i, array);
+    myArr.push(result);
+
+}
+return myArr
 };
 
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
 
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 
 
